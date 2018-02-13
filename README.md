b0c4454e10a079d60c2e9b432ad33bff95f811de
brunston <brunston@protonmail.com>
2018-02-12 21:46:23 -0800

    beginnings

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+Insert Fitbit API tokens into a file called `fitbit_client_tokens.pri` as follows:
+
+```
+<CLIENT ID GOES HERE>
+<CLIENT SECRET GOES HERE>
+```
---
+++ get_tokens.py
@@ -0,0 +1,35 @@
+"""get_tokens
+
+Spins up a server to get user tokens from Fitbit.
+"""
+
+try:
+    from flask import Flask
+    from fitbit.api import Fitbit
+except ImportError:
+    raise PythonBitVisException('python-bitvis requires flask, python-fitbit')
+
+class AuthenticationServer:
+    def __init__(self,
+                 fb_client_id,
+                 fb_client_secret,
+                 redirect_url='http://127.0.0.1:8080'):
+        """Initialize an OAuth2 client using python-fitbit"""
+        self.success = """<h1>Fitbit authentication Successful!</h1>"""
+
+        self.fitbit = Fitbit(fb_client_id, fb_client_secret,
+                             redirect_uri=redirect_url,
+                             timeout=15)
+        
+
+if __name__ == '__main__':
+    client_tokens = []
+    try:
+        with open('fitbit_client_tokens.pri') as f:
+            client_tokens = [token.strip() for token in f.readlines()]
+    except FileNotFoundError:
+        raise PythonBitVisException('fitbit_clients_tokens.pri not found. '
+                                    'Consult the README')
+
+class PythonBitVisException(Exception):
+    """python-bitvis exception"""
