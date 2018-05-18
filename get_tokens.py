2caab911bfb0309281e875766cf76fa6200d6301
brunston <brunston@protonmail.com>
2018-05-17 22:31:38 -0700

    180517 this is work in progress, switching to handling grabbing oauth and login stuff so that I can learn

--- get_tokens.py
+++ get_tokens.py
@@ -5,9 +5,19 @@ Spins up a server to get user tokens from Fitbit.
 
 try:
     from flask import Flask
+    from flask_sqlalchemy import SQLAlchemy
+    
     from fitbit.api import Fitbit
+    
 except ImportError:
-    raise PyBitVisException('py-bitvis requires flask, python-fitbit')
+    raise ImportError('py-bitvis requires flask, python-fitbit, pymongo')
+
+client_tokens = []
+try:
+    with open('fitbit_client_tokens.pri') as f:
+        client_tokens = [token.strip() for token in f.readlines()]
+except FileNotFoundError:
+    raise PyBitVisException('fitbit_clients_tokens.pri not found. Consult readme.')
 
 class AuthenticationServer:
     def __init__(self,
@@ -21,15 +31,5 @@ class AuthenticationServer:
                              redirect_uri=redirect_url,
                              timeout=15)
     
-
-if __name__ == '__main__':
-    client_tokens = []
-    try:
-        with open('fitbit_client_tokens.pri') as f:
-            client_tokens = [token.strip() for token in f.readlines()]
-    except FileNotFoundError:
-        raise PyBitVisException('fitbit_clients_tokens.pri not found. '
-                                'Consult the README')
-
 class PyBitVisException(Exception):
     """py-bitvis exception"""
